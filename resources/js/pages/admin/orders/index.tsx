7973a58704d09449d8e5a50d98e9f0f8c5bd4dd7
Mohamed Esmail <dev.mohamed.esmail@gmail.com>
2025-04-20 21:25:25 +0400

    update

--- resources/js/pages/admin/orders/index.tsx
+++ resources/js/pages/admin/orders/index.tsx
@@ -22,17 +22,16 @@ export default function Orders({ orders }: any) {
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Categories" />
             <div className='px-5'>
-                <div className="overflow-x-auto">
+            <div className="overflow-x-auto">
                     <table className="table">
-                        
+
                         <thead>
                             <tr className='bg-primary'>
-                                
+
                                 <th className='text-black'>Table No</th>
                                 <th className='text-black'>Order</th>
                                 <th className='text-black'>Total</th>
-                                <th className='text-black'>Status</th>
-                                <th className='text-black'>Actions</th>
+                              
                             </tr>
                         </thead>
                         <tbody>
@@ -42,28 +41,37 @@ export default function Orders({ orders }: any) {
 
                                 return (
                                     <tr key={order.id} className='border'>
-                                       
+
                                         <td >
-                                            
-                                            <p className='w-8 h-8 bg-primary flex justify-center items-center rounded-full'>{order.table}</p>
+                                            {
+                                                order.table ? (
+                                                    <p className='w-8 h-8 bg-primary flex justify-center items-center rounded-full'>{order.table}</p>
+                                                ) : (
+                                                    <div>
+                                                        <p>{order.address}</p>
+                                                        <p>{order.phone}</p>
+                                                        <p>{order.name}</p>
+                                                    </div>)
+                                            }
+
+
                                         </td>
                                         <td>
                                             {items.map((item: any, index: number) => (
-                                                <div key={index} className='flex justify-between'>
-                                                    
-                                                    <div>
-                                                    <img className='w-16 h-16 rounded-full' src={`/uploads/${item.image}`} alt={item.name_ar} />
-                                                    <p>{item.name_ar}</p>
+                                                <div key={index} className='flex justify-between items-center'>
+
+                                                    <div className='flex items-center'>
+                                                        <img className='w-16 h-16 rounded-full' src={`/uploads/${item.image}`} alt={item.name_ar} />
+                                                        <p className='px-2'>{item.name_ar}</p>
                                                     </div>
                                                     <p className='font-bold bg-primary w-5 h-5 flex justify-center items-center rounded-2xl'>{item.quantity}</p>
                                                 </div>
                                             ))}
                                         </td>
                                         <td>{order.total}</td>
-                                        <td>{order.status}</td>
-                                        <th>
-                                            <button className="btn btn-ghost btn-xs">details</button>
-                                        </th>
+                                       
+
+                                       
                                     </tr>
                                 );
                             })}
